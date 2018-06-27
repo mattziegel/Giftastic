b7f9524b87ef840acebd2db1fc5ae28a9fef50ea
Lefty Lang <kitchensink@Kitchens-MacBook-Pro.local>
2018-06-27 17:15:53 -0400

    thanks julia

--- api.js
+++ api.js
@@ -1,8 +1,34 @@
-var topics = ["Cat", "Tacos", "Dog", "High Five", "Betty White", "Karate", "Pizza", "Samuel L. Jackson", "Earth", "Columbo", "Football", "Daffy Duck", "Nope", "Banana", "Basketball", "Spacesip", "Shark", "Owl", "Dancing", "Fireworks", "Ice Cream", "Pugs", "Waves", "Math", "Wink", "Wood Turning", "Panda", "Laughing", "Raccoon", "Crying"];
-console.log(topics);
+$(document).ready(function() {
 
-$("#buttons").on("click", function() {
+  var topics = ["Cat", "Tacos", "Dog", "High Five", "Betty White", "Karate", "Pizza", "Samuel L Jackson", "Earth", "Columbo", "Football", "Daffy Duck", "Nope", "Banana", "Basketball", "Spacesip", "Shark", "Owl", "Dancing", "Fireworks", "Ice Cream", "Pugs", "Waves", "Math", "Wink", "Wood Turning", "Panda", "Laughing", "Raccoon", "Crying"];
+  console.log(topics);
+
+  function renderButtons() {
+    $("#buttons").empty();
+    for (var i = 0; i < topics.length; i++) {
+      var button = $("<button>");
+      button.addClass("gif-button");
+      button.attr("data-name", topics[i]);
+      button.text(topics[i]);
+      $("#buttons").append(button);
+    };
+  };
+  
+  $("#add-thing").on("click", function (event) {
+    event.preventDefault();
+    var thing = $("#thing-input").val().trim();
+    if (!thing) { return };
+    topics.push(thing);
+    $("#thing-input").val("");
+    renderButtons();
+
+    $("button").on("click", gifSearch)
+  });
+
+
+  function gifSearch() {
   var gif = $(this).attr("data-name");
+  // var gifStr = gif.split(" ").join("+");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     gif + "&api_key=Gtl8YGKTa0pryKVfcYHYpDP7oD4ek6Hx&&s&limit=10";
 
@@ -14,55 +40,47 @@ $("#buttons").on("click", function() {
     .then(function(response) {
       var results = response.data;
       console.log(results);
+
       for (var i = 0; i < results.length; i++) {
         var rating = results[i].rating;
+
         var p = $("<p>").text("Rating: " + rating);
         var gifImage = $("<img>");
-        gifImage.addClass("gif");
-        gifImage.attr("src", results[i].images.fixed_height_still.url);
-        gifImage.attr("data-still", results[i].images.fixed_height_still.url);
-        gifImage.attr("data-animate", results[i].images.fixed_height.url);
+
+        var gifStill = (results[i].images.fixed_height_still.url);
+        var gifAnimate = (results[i].images.fixed_height.url);
+
+        gifImage.attr("src", gifStill);
         gifImage.attr("data-state", "still");
+
         $("#gifs").prepend(p);
         $("#gifs").prepend(gifImage);
       }
+
+      $("img").on("click", function() {
+        var state = $(this).attr("data-state");
+    
+        console.log (state)
+    
+        if (state ==="still") {
+            $(this).attr("data-state", "animate");
+            gifImage.attr("src", gifAnimate);
+        }
+    
+        else if (state==="animate") {
+            $(this).attr("data-state", "still");
+            gifImage.attr("src", gifStill);
+        }
     })
 
-    $(".gif").on("click", function() {
-      var state = $(this).attr("data-state");
-      console.log(state);
+  })
 
-      if (state === "still") {
-        $(this).attr("src", $(this).data("animate"));
-        $(this).attr("data-state", "animate");
-      } 
-      
-      else if (state === "animate") {
-        $(this).attr("src", $(this).data("still"));
-        $(this).attr("data-state", "still");
-      }
-    })
+};
+
+renderButtons();
+
+$(document).on("click", ".gif-button", gifSearch);
 
 });
 
-function renderButtons() {
-  $("#buttons").empty();
-  for (var i = 0; i < topics.length; i++) {
-    var button = $("<button>");
-    button.addClass("gif-button");
-    button.attr("data-name", topics[i]);
-    button.text(topics[i]);
-    $("#buttons").append(button);
-  }
-}
-
-$("#add-thing").on("click", function (event) {
-  event.preventDefault();
-  var thing = $("#thing-input").val().trim();
-  if (!thing) { return };
-  topics.push(thing);
-  renderButtons();
-})
-
-renderButtons();
 
