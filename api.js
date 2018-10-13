061271d311ff2a15f33ee3f94cb83b1ceaa224bf
Lefty Lang <kitchensink@Kitchens-MacBook-Pro.local>
2018-10-13 02:59:13 -0400

    revamp

--- api.js
+++ api.js
@@ -42,34 +42,34 @@ $(document).ready(function() {
       console.log(results);
 
       for (var i = 0; i < results.length; i++) {
-        var rating = results[i].rating;
+        // var rating = results[i].rating;
 
-        var p = $("<p>").text("Rating: " + rating);
+        // var p = $("<p>").text("Rating: " + rating);
         var gifImage = $("<img>");
 
-        gifImage.attr("src", results[i].images.fixed_height_still.url);
+        gifImage.attr("src", results[i].images.fixed_height.url);
 -       gifImage.attr("data-state");
 +       gifImage.attr("data-still", results[i].images.fixed_height_still.url);
 +       gifImage.attr("data-animate", results[i].images.fixed_height.url);
-+       gifImage.attr("data-state", "still");
++       gifImage.attr("data-state", "animate");
 
-        $("#gifs").prepend(p);
+        // $("#gifs").prepend(p);
         $("#gifs").prepend(gifImage);
       }
 
       $("img").on("click", function() {
         var state = $(this).attr("data-state");
     
-        console.log (state)
+        console.log (state);
     
-        if (state === "still") {
-            $(this).attr("src", $(this).data("animate"));
-            $(this).attr("data-state", "animate");
-        }
-    
-        else if (state === "animate") {
+        if (state === "animate") {
             $(this).attr("src", $(this).data("still"));
             $(this).attr("data-state", "still");
+        }
+    
+        else if (state === "still") {
+            $(this).attr("src", $(this).data("animate"));
+            $(this).attr("data-state", "animate");
         }
     })
 
